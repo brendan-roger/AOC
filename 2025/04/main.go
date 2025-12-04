6c764d314b0cae2ffa5be8955bada940782ef41a
Brendan Elton <brendan.elton@gmail.com>
2025-12-05 00:41:48 +1100

    day-4 part-2

--- 2025/04/main.go
+++ 2025/04/main.go
@@ -8,37 +8,11 @@ import (
 func main() {
 	data := util.ReadLines("2025/04/input.txt")
 	f.Println("Part 1:", part1(data))
-	// f.Println("Part 2:", part(data, 12))
+	f.Println("Part 2:", part2(data))
 }
 
-// 00@@.@@@@.
-// @@@.@.@.@@
-// @@@@@.@.@@
-// @.@@@@..@.
-// @@.@@@@.@@
-// .@@@@@@@.@
-// .@.@.@.@@@
-// @.@@@.@@@@
-// .@@@@@@@@.
-// @.@.@@@.@.
+func getPrefixMap(matrix []string) [][]int {
 
-// [
-//  [0 0 1 2 2 3 4 5 6 6]
-//  [1 2 4 5 6 7 9 10 12 13]
-//  [2 4 7 9 11 12 15 16 19 21]
-//  [3 5 9 12 15 17 20 21 25 27]
-//  [4 7 11 15 19 22 26 27 32 35]
-//  [4 8 13 18 23 27 32 34 39 43]
-//  [4 9 14 20 25 30 35 38 44 49]
-//  [5 10 16 23 29 34 40 44 51 57]
-//  [5 11 18 26 33 39 46 51 59 65]
-//  [6 12 20 28 36 43 51 56 65 71]
-// ]
-
-
-func part1(matrix []string) int {
-
-	// can only access 4 < in 8 adjacent positions
 	rows := len(matrix)
 	cols := len(matrix[0])
 
@@ -46,13 +20,6 @@ func part1(matrix []string) int {
 
 	for i := range prefixMap {
 		prefixMap[i] = make([]int, cols)
-	}
-
-	get := func(row, col int) int {
-		if row < 0 || col < 0 {
-			return 0
-		}
-		return prefixMap[row][col]
 	}
 
 	for i := 0; i < rows; i++ {
@@ -84,6 +51,23 @@ func part1(matrix []string) int {
 		}
 	}
 
+	return prefixMap
+}
+
+func part1(matrix []string) int {
+
+	// can only access 4 < in 8 adjacent positions
+	rows := len(matrix)
+	cols := len(matrix[0])
+	prefixMap := getPrefixMap(matrix)
+
+	get := func(row, col int) int {
+		if row < 0 || col < 0 {
+			return 0
+		}
+		return prefixMap[row][col]
+	}
+
 	res := 0
 
 	for i := 0; i < rows; i++ {
@@ -97,18 +81,114 @@ func part1(matrix []string) int {
 			col0 := max(j-1, 0)
 			col2 := min(j+1, cols-1)
 
-			total :=
+			// prefix sum for 3x3 - 1 for the center cell
+			count :=
 				get(row2, col2) -
-				get(row0-1, col2) -
-				get(row2, col0-1) +
-				get(row0-1, col0-1)
+					get(row0-1, col2) -
+					get(row2, col0-1) +
+					get(row0-1, col0-1) -
+					1
 
-
-			if total-1 < 4 {
+			if count < 4 {
 				res += 1
 			}
 
 		}
+	}
+
+	return res
+}
+
+type Cell struct {
+	row int
+	col int
+}
+
+func part2(matrix []string) int {
+
+	rows := len(matrix)
+	cols := len(matrix[0])
+	prefixMap := getPrefixMap(matrix)
+	var queue = []Cell{}
+
+	get := func(row, col int) int {
+		if row < 0 || col < 0 {
+			return 0
+		}
+		return prefixMap[row][col]
+	}
+
+	neighborMap := make([][]int, rows)
+
+	for i := range prefixMap {
+		neighborMap[i] = make([]int, cols)
+	}
+
+	// Make a matrix with the number of "@" neighbours a cell has
+	// If less that 4 add to queue
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+
+			if matrix[i][j] != '@' {
+				continue
+			}
+
+			row0 := max(i-1, 0)
+			row2 := min(i+1, rows-1)
+			col0 := max(j-1, 0)
+			col2 := min(j+1, cols-1)
+
+			count :=
+				get(row2, col2) -
+					get(row0-1, col2) -
+					get(row2, col0-1) +
+					get(row0-1, col0-1) -
+					1
+
+			if count < 4 {
+				queue = append(queue, Cell{row: i, col: j})
+				neighborMap[i][j] = -1
+			} else {
+				neighborMap[i][j] = count
+			}
+
+		}
+	}
+
+	res := 0
+
+	// Iterate over queue and decrement neighbours.
+	// If after decrementing neighbour is less than 4 enqueue cell
+	for len(queue) != 0 {
+
+		cell := queue[len(queue)-1]
+		queue = queue[:len(queue)-1]
+
+		operations := [][]int{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, 0}, {1, -1}, {1, 1}}
+
+		for _, op := range operations {
+
+			row := cell.row + op[0]
+			col := cell.col + op[1]
+
+			if row < 0 || col < 0 || row >= rows || col >= cols || matrix[row][col] != '@' {
+				continue
+			}
+
+			next := neighborMap[row][col]
+
+			if next == 4 {
+				queue = append(queue, Cell{row, col})
+				neighborMap[row][col] = -1
+			}
+
+			if next > 4 {
+				neighborMap[row][col] -= 1
+			}
+
+		}
+
+		res += 1
 	}
 
 	return res
